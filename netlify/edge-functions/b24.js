a7ea2aa70a5073b60ad8dd2e22e9776a2d273822
serg35361-afk <serg35361@gmail.com>
2026-09-25 11:52:21 +0300

    Add edge function to handle Bitrix24 POST requests
    Intercept POST requests from Bitrix24 and rewrite to index.html

--- netlify/edge-functions/b24.js
+++ netlify/edge-functions/b24.js
@@ -1,1 +1,7 @@
-
+export default async (request, context) => {
+  // Перехватываем POST от Битрикс24 и отдаем index.html как GET
+  const url = new URL(request.url);
+  url.pathname = "/index.html";
+  
+  return context.rewrite(url.toString());
+};
