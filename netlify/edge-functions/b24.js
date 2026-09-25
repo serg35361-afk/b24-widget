b1efc7c2f9fbe2e40861420aa55e6eac27855789
serg35361-afk <serg35361@gmail.com>
2026-09-25 11:55:12 +0300

    Modify request handling to use GET for index.html
    Change POST request handling to force GET request for index.html.

--- netlify/edge-functions/b24.js
+++ netlify/edge-functions/b24.js
@@ -1,7 +1,11 @@
 export default async (request, context) => {
-  // Перехватываем POST от Битрикс24 и отдаем index.html как GET
   const url = new URL(request.url);
   url.pathname = "/index.html";
-  
-  return context.rewrite(url.toString());
+
+  // Принудительно запрашиваем index.html методом GET
+  const response = await context.rewrite(url.toString(), {
+    method: "GET"
+  });
+
+  return response;
 };
